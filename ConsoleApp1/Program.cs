1e7bfa6afce213878b611c4efeb68a97368f483f
Leonardo H. R. Paulino <leonardo@lhrp.com.br>
2024-09-11 18:37:22 -0300

    Att

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Designer forms , executar o comando abaixo no terminal
+//dotnet new winforms
+
+// See https://aka.ms/new-console-template for more information
+
+//Tipos de Dados no C#
+int nParcelas = 6;
+float nValorFloat = 129.89f;
+double nValorDouble = 126.89d;
+decimal nValorDecimal = 125.89m;
+//Quando a precisão das casas decimais não forem muito relevantes, utilizar o Float ou Double
+//Quando a precisão das casas decimais for importante, utilizar o decimal
+
+/*
+Imprime no temrinal, sem incluir quebras no final, onde se utilizado vários sem tratamento, serão impressos os dados todos juntos
+Console.Write("");
+Imprime no terminal, já incluindo uma quebra de linha
+Console.WriteLine("");
+*/
+
+Console.WriteLine(nParcelas);
+Console.WriteLine(nValorFloat);
+Console.WriteLine(nValorDouble);
+Console.WriteLine(nValorDecimal);
+
